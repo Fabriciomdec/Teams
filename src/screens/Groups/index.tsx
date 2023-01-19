17de1d0640747e2862f0fcd5951ec37f8a9028a2
Fabricio Menezes de Carvalho <fabriciomdec@gmail.com>
2023-01-19 10:16:40 -0300

    feat: :sparkles: create default message for empty list

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
+import { FlatList } from "react-native";
 
 import { GroupCard } from "@components/GroupCard";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import * as Group from "./styles";
+import { ListEmpty } from "@components/ListEmpty";
 
 export function Groups() {
+  const [groups, setGroups] = useState<string[]>([]);
+
   return (
     <Group.Container>
-      <Header/>
-      <Highlight title="Turmas" subtitle="jogue com a sua turma"/>
-
-      <GroupCard title="Turma do Pagode"/>
+      <Header />
+      <Highlight title="Turmas" subtitle="jogue com a sua turma" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => <GroupCard title={item} />}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Não possui nenhuma turma..." />
+        )}
+      />
     </Group.Container>
   );
 }
