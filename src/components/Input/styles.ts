438f0a009444b4cc9e762961981874c9972daf44
Fabricio Menezes de Carvalho <fabriciomdec@gmail.com>
2023-01-19 11:38:31 -0300

    refactor: :recycle: refactor theme of components

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,18 +1,20 @@
 import { TextInput } from "react-native";
-import styled from "styled-components/native";
+import styled, { css } from "styled-components/native";
 
 export const Container = styled(TextInput)`
-flex: 1;
+  ${({ theme }) => css`
+    color: ${theme.COLORS.WHITE};
+    background-color: ${theme.COLORS.GRAY_700};
 
-min-height: 56px;
-max-height: 56px;
+    font-family: ${theme.FONT_FAMILY.REGULAR};
+    font-size: ${theme.FONT_SIZE.MD}px;
+  `}
 
-color: ${({theme}) => theme.COLORS.WHITE};
-background-color: ${({theme }) => theme.COLORS.GRAY_700};
+  flex: 1;
 
-font-family: ${({theme}) => theme.FONT_FAMILY.REGULAR};
-font-size: ${({theme}) => theme.FONT_SIZE.MD}px;
+  min-height: 56px;
+  max-height: 56px;
 
-border-radius: 6px;
-padding: 16px;
+  border-radius: 6px;
+  padding: 16px;
 `;
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -15,7 +15,7 @@ export function NewGroup() {
           title="Nova Turma"
           subtitle="Crie a turma para adicionar as pessoas"
         />
-        <Input />
+        <Input placeholder="Digite o nome da turma"/>
         <Button title="Criar" style={{marginTop: 20}}/>
       </StyledNewGroup.Content>
     </StyledNewGroup.Container>
