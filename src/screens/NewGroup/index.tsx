cf509a0a031eb2dc10fa861ef4adeb630393f2d8
Fabricio Menezes de Carvalho <fabriciomdec@gmail.com>
2023-01-24 16:12:07 -0300

    feat: :sparkles: save groups on async storage

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -7,6 +7,8 @@ import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
 import { groupCreate } from "@storage/group/groupCreate";
+import { AppError } from "@utils/AppError";
+import { Alert } from "react-native";
 
 export function NewGroup() {
   const [group, setGroup] = useState<string>("");
@@ -14,11 +16,18 @@ export function NewGroup() {
   const navigation = useNavigation();
 
   async function handleNewGroup() {
+    if (group.trim().length === 0) {
+      return Alert.alert("Novo Grupo", "Informe o nome da turma");
+    }
     try {
       await groupCreate(group);
       navigation.navigate("players", { group });
     } catch (error) {
-      console.log(error);
+      if (error instanceof AppError) {
+        Alert.alert("Novo Grupo", error.message);
+      } else {
+        Alert.alert("Novo Grupo", "Não foi possível criar um novo grupo.");
+      }
     }
   }
 
