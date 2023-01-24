25a0916c4944a97a2e74c8b8f6d68f65a401c22c
Fabricio Menezes de Carvalho <fabriciomdec@gmail.com>
2023-01-24 12:02:37 -0300

    feat: :sparkles: add types for navigation

--- App.tsx
+++ App.tsx
@@ -3,8 +3,9 @@ import { ThemeProvider } from 'styled-components';
 import { useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto'
 
 import theme from './src/theme/theme';
+import { Loading } from '@components/Loading';
 
-import { Routes } from 'src/routes';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({Roboto_400Regular, Roboto_700Bold});
